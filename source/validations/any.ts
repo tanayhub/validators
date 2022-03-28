a9eb0938fd2ef01951102180e0a4c11c2bea12dd
Tanay Ghirnikar <tanay.ghirnikar@tanayhub.com>
2022-03-28 11:12:42 +0530

    clean repository

--- source/validations/any.ts
+++
@@ -1,8 +0,0 @@
-import { ValidationFunction } from ".";
-import AnyDefinition from "../definitions/any";
-
-export default function compileAny(definition: AnyDefinition): ValidationFunction {
-    return (payload: any): string[] => {
-        return [];
-    };
-}
