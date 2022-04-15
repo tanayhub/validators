e2c6d9dd0e2fce64b1d87ba35a3b0d4fd2701985
Tanay Ghirnikar <tanay.ghirnikar@tanayhub.com>
2022-04-15 17:18:20 +0530

    mkae v1 ready

--- source/models/general.ts
+++
@@ -1,9 +0,0 @@
-export type ForcedArray<Type> = [Type, Type, ...Type[]];
-
-export type Hybrid<Type> = Type | ForcedArray<Type>;
-
-export type ValidationFunction = (payload: any) => true | string[];
-
-export interface ValidatorInterface {
-    validate(payload: any): null | string[];
-}
