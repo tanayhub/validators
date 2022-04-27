258295ba6e5c4dae87ea4706ac4ddb0dd89c1012
Tanay Ghirnikar <tanay.ghirnikar@tanayhub.com>
2022-04-27 12:11:30 +0530

    correct linting

--- source/errors/index.ts
+++ source/errors/index.ts
@@ -1,12 +1,13 @@
 export default class ValidationError extends Error {
   public readonly name: string = "ValidationError";
   public readonly type: string = "unknown";
-  public path: string = "";
+  public path: string;
   public readonly received: string;
   public readonly expected: string;
 
   constructor(received: any, expected: any) {
     super();
+    this.path = "";
     this.received = JSON.stringify(received) ?? received.toString();
     this.expected = JSON.stringify(expected) ?? expected.toString();
   }
